f993a9c00e6d86cde481886127c0feacf6725bbe
hhh <1340545944@qq.com>
2020-05-04 10:11:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,73 @@
+# create-3h-app
+
+> A simple app initializer.
+
+## Introduction
+
+This is a CLI tool that helps you quickly initialize a development environment for your awesome front-end app.
+
+## Usage
+
+```txt
+$ create-3h-app --help
+A front-end app initializer.
+
+Usage:
+  create-3h-app [options]
+
+Options:
+  --name, -n <pkg>              The name of the package
+  --author, -a <name>           The author of the package
+  --desc, -d <description>      The description of the package
+  --keywords, -k <words...>     The keywords of the package
+  --repo, -r <repository>       The repository of the package
+  --no-install                  Do not install dependencies instantly
+  --help, -h                    Show help info
+
+```
+
+## Example
+
+```bash
+npm init 3h-app -n my-awesome-app -a Peter -d "This is my awesome lib"
+# or
+npx create-3h-app -n my-awesome-app -a Peter -d "This is my awesome lib"
+```
+
+## Template Structure
+
+```txt
+your-awesome-app/
++-- public/
+| +-- index.html
+| `-- index.css
++-- src/
+| `-- index.js
++-- test/
+| `-- index.html
++-- .gitignore
++-- LICENSE
++-- README.md
++-- rollup.config.js
+`-- dev-server.js
+```
+
+## Workflow
+
+Generally, you
+
+1. Write your source code in the `src` folder
+2. Test your app using the dev server(by executing `npm test`) which loads your source code directly
+3. Build your app by executing `npm run build`
+4. Deploy your app in the `public` directory
+
+## Built-in Scripts
+
+| name    | description           |
+|:--------|:----------------------|
+| `test`  | launch the dev server |
+| `build` | build your code       |
+
+The development server employs [`herver`](https://github.com/huang2002/herver) to serve your app for testing. It will serve your source code directly from `src`, so after you change something in your source code, you can just refresh the page to test it. Additionally, any other resource files are supposed to be put in the `public` folder, and they will be served properly.
+
+After building your code by executing `npm run build`, a bundled JavaScript file(`index.js`) will be placed in the `public` folder, which will be transformed by `babel`, bundled by `rollup` and minified by `terser`. Finally, you will get your app in the `public` folder.
---
+++ index.js
@@ -0,0 +1,198 @@
+#!/usr/bin/env node
+// @ts-check
+const { Program } = require('3h-cli'),
+    { existsSync, promises: fsPromises } = require('fs'),
+    { execSync } = require('child_process'),
+    { join } = require('path'),
+    // @ts-ignore
+    pkgInfo = require('./package.json');
+
+/* Template data slot: __var__ */
+
+const TEMPLATE_PATH = './template',
+    TEMPLATE_ENCODING = 'utf8';
+
+const templateDirectories = [
+    'public',
+    'src',
+    'test',
+];
+
+const templateFiles = [
+    'src/index.js',
+    'test/index.html',
+    'public/index.html',
+    'public/index.css',
+    'LICENSE',
+    'README.md',
+    'rollup.config.js',
+    'dev-server.js',
+];
+
+/**
+ * @typedef TemplateData
+ * @property {string} name
+ * @property {string} desc
+ * @property {string} author
+ * @property {string} year
+ */
+
+/**
+ * @param {TemplateData} data
+ */
+const renderTemplates = async data => {
+    await templateDirectories.reduce(
+        (prevPromise, dir) => prevPromise.then(() => fsPromises.mkdir(dir)),
+        Promise.resolve()
+    );
+    await Promise.all(
+        templateFiles.map(async path => {
+            let content = await fsPromises.readFile(
+                join(__dirname, TEMPLATE_PATH, path),
+                TEMPLATE_ENCODING
+            );
+            Object.entries(data).forEach(([key, value]) => {
+                content = content.replace(
+                    new RegExp(`__${key}__`, 'g'),
+                    value
+                );
+            });
+            await fsPromises.writeFile(path, content);
+        })
+    );
+};
+
+const program = new Program(pkgInfo.name, {
+    title: pkgInfo.description
+});
+
+program
+    .option({
+        name: '--name',
+        alias: '-n',
+        value: '<pkg>',
+        help: 'The name of the package'
+    })
+    .option({
+        name: '--author',
+        alias: '-a',
+        value: '<name>',
+        help: 'The author of the package'
+    })
+    .option({
+        name: '--desc',
+        alias: '-d',
+        value: '<description>',
+        help: 'The description of the package'
+    })
+    .option({
+        name: '--keywords',
+        alias: '-k',
+        value: '<words...>',
+        help: 'The keywords of the package'
+    })
+    .option({
+        name: '--repo',
+        alias: '-r',
+        value: '<repository>',
+        help: 'The repository of the package'
+    })
+    .option({
+        name: '--no-install',
+        help: 'Do not install dependencies instantly'
+    })
+    .option({
+        name: '--help',
+        alias: '-h',
+        help: 'Show help info'
+    })
+    .parse(process.argv)
+    .then(async args => {
+
+        const { options } = args;
+
+        if (options.has('--help')) {
+            return program.help();
+        }
+
+        if (!options.has('--name')) {
+            throw 'Package name is not provided';
+        }
+
+        const name = options.get('--name')[0];
+
+        if (existsSync(name)) {
+            throw `Path "${name}" already exists`;
+        }
+
+        if (!options.has('--author')) {
+            throw 'Package author is not provided';
+        }
+
+        const data = {
+            name,
+            desc: args.getOption('--desc').join(' ') || `This is ${name}.`,
+            author: options.get('--author')[0],
+            year: (new Date()).getFullYear() + '',
+        };
+
+        await fsPromises.mkdir(name);
+        process.chdir(name);
+
+        console.time('time used');
+
+        console.log('Generating files...');
+        await renderTemplates(data);
+        /**
+         * it seems npm will somehow convert .gitignore
+         * into .npmignore when installing the package,
+         * so this file is added manually
+         */
+        await fsPromises.writeFile('.gitignore', [
+            'node_modules',
+            'public',
+        ].join('\n') + '\n');
+        await fsPromises.writeFile('package.json', JSON.stringify(
+            {
+                name,
+                version: '0.1.0',
+                description: data.desc,
+                author: data.author,
+                license: 'MIT',
+                private: true,
+                scripts: {
+                    build: 'rollup -c',
+                    test: 'node dev-server',
+                },
+                repository: args.getOption('--repo')[0]
+                    || `${data.author}/${name}`,
+                keywords: args.getOption('--keywords'),
+                devDependencies: {
+                    '@babel/core': '^7.9.0',
+                    '@babel/preset-env': '^7.9.0',
+                    herver: '^0.4.0',
+                    rollup: '^2.2.0',
+                    'rollup-plugin-babel': '^4.3.0',
+                    'rollup-plugin-terser': '^5.3.0',
+                    terser: '^4.6.0',
+                }
+            },
+            null, // replacer
+            2 // indent
+        ));
+
+        if (options.has('--no-install')) {
+            console.log('Dependencies not installed.');
+        } else {
+            console.log('Installing dev dependencies...');
+            execSync('npm i', { stdio: 'inherit' });
+        }
+
+        console.log('Finished!');
+        console.timeEnd('time used');
+
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
